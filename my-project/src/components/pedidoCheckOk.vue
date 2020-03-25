50dcec1e5f96c27f902448ce9e843047a3c96d1c
rubialburqueque <rubi.vap@gmail.com>
2020-03-25 18:28:16 -0500

    vista model

--- my-project/src/components/pedidoCheckOk.vue
+++ my-project/src/components/pedidoCheckOk.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-
-
 <!-- Modal -->
 <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-scrollable" role="document">
@@ -51,10 +49,7 @@
   </div>
 </div>
 
-
-
-     
-  </div>
+</div>
 </template>
 
 <script>
